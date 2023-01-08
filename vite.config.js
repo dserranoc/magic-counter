748a0f87e6daaa668852b1de2fb978802807606d
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-01-08 21:19:19 +0100

    Test: PWA

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,39 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), VitePWA({ registerType: 'autoUpdate', manifest: { display: "fullscreen" } })]
+  plugins: [
+    preact(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        display: 'fullscreen',
+        name: 'Magic Counter',
+        short_name: 'MagicCounter',
+        theme_color: '#ffffff',
+        start_url: '/',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/assets/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
