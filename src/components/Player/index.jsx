a229a34edba924be947a2a55b3aef1570d3dfd68
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-01-08 22:09:40 +0100

    WIP New mode

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -3,10 +3,20 @@ import Counter from '../Counter'
 import MiniCounter from '../MiniCounter'
 import GameContext from '../../contexts/GameContext'
 export function Player ({ playerId }) {
-  const [lives, setLives] = useState(40)
-  const [poison, setPoison] = useState(0)
+  const { playersNumber, styles, gamemode } = useContext(GameContext)
 
-  const { playersNumber, styles } = useContext(GameContext)
+  const DEFAULT_LIVES = 40
+  const DEFAULT_POISON = 0
+  const MIN_VALUE = -2
+  const MAX_VALUE = 200
+
+  const DEFAULT_MINI_VALUE = 0
+  const MINI_MIN_VALUE = 0
+  const MINI_MAX_VALUE = 21
+
+  const [lives, setLives] = useState(DEFAULT_LIVES)
+  const [poison, setPoison] = useState(DEFAULT_POISON)
+
   const playerStyles = styles.players[playerId - 1]
   const spanStyles = playerStyles.span
   const rotationStyles = playerStyles.rotation
@@ -17,14 +27,14 @@ export function Player ({ playerId }) {
     <div id={playerId} className={`${playerStyles.color} ${spanStyles && spanStyles[playersNumber] ? spanStyles[playersNumber] : ''} flex place-content-center items-center`}>
       <div className={`grid overflow-hidden grid-rows-2 grid-cols-2 ${rotationStyles && rotationStyles[playersNumber] ? rotationStyles[playersNumber] : ''}`}>
         <div className='flex items-center place-content-center'>
-          <Counter minValue={-2} maxValue={200} defaultValue={40} setValue={setLives} value={lives} />
+          <Counter minValue={MIN_VALUE} maxValue={MAX_VALUE} defaultValue={DEFAULT_LIVES} setValue={setLives} value={lives} />
         </div>
         <div className='flex items-center place-content-center'>
-          <Counter minValue={-2} maxValue={200} defaultValue={40} setValue={setPoison} value={poison} style='bg-white/50 rounded-md' />
+          <Counter minValue={MIN_VALUE} maxValue={MAX_VALUE} defaultValue={DEFAULT_LIVES} setValue={setPoison} value={poison} style='bg-white/50 rounded-md' />
         </div>
         <div className='col-span-2'>
           <div className='inline-grid overflow-hidden grid-rows-2 grid-cols-3 gap-3'>
-            {otherPlayers.map((player) => <MiniCounter key={player.id} playerId={player.id} minValue={0} maxValue={21} defaultValue={0} setLives={setLives} lives={lives} />)}
+            {otherPlayers.map((player) => <MiniCounter key={player.id} playerId={player.id} minValue={MINI_MIN_VALUE} maxValue={MINI_MAX_VALUE} defaultValue={DEFAULT_MINI_VALUE} setLives={setLives} lives={lives} />)}
           </div>
         </div>
       </div>
--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,6 +1,17 @@
+import { useState, useContext, useEffect } from 'preact/hooks'
 import Board from '../../components/Board'
+import GameContext from '../../contexts/GameContext'
+import { useLocation } from 'wouter-preact'
 
 export default function Game () {
+  const { playersNumber, gamemode } = useContext(GameContext)
+  const [, navigate] = useLocation()
+
+  useEffect(() => {
+    if (!playersNumber || !gamemode) {
+      navigate('/')
+    }
+  }, [playersNumber, gamemode])
   return (
     <Board />
   )
